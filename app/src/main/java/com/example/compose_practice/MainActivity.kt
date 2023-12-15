48d28076034e65408dd5aa7f08929c1dcc50709f
추민수 <devsoftychoo@gmail.com>
2023-12-15 13:49:01 +0900

    [Feat] : rememberCoroutineScope, keyboardController, TextField, Scaffold, SnackBar

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -1,136 +1,90 @@
 package com.example.compose_practice
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose_practice.ui.theme.ComposepracticeTheme
+import kotlinx.coroutines.launch
 
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var isFavorite by rememberSaveable {
-                mutableStateOf(false)
+            val (text, setValue) = rememberSaveable {
+                mutableStateOf("")
             }
-            ImageCard(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(16.dp),
-                isFavorite = isFavorite
-            ) { favorite ->
-                isFavorite = favorite
-            }
-        }
-    }
-}
+            val scaffoldState = rememberScaffoldState() //최근의 상태를 저장
+            val scope = rememberCoroutineScope()
+            val keyboardController = LocalSoftwareKeyboardController.current
 
-@Composable
-fun ImageCard(
-    modifier: Modifier = Modifier,
-    isFavorite: Boolean,
-    onTabFavorite: (Boolean) -> Unit,
-) {
-    Card(
-        modifier = modifier,
-        shape = RoundedCornerShape(8.dp),
-    ) {
-        Box(
-            modifier = Modifier.height(200.dp)
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.choo),
-                contentDescription = "image",
-                contentScale = ContentScale.Crop
-            )
-            IconButton(
-                onClick = {
-                    onTabFavorite(!isFavorite)
-                },
-                modifier = Modifier.align(Alignment.TopEnd)
+            androidx.compose.material.Scaffold(
+                scaffoldState = scaffoldState
             ) {
-                Icon(
-                    imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
-                    contentDescription = "favorite",
-                    tint = Color.White
-                )
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextField(
+                        value = text,
+                        onValueChange = setValue
+                    )
+                    Button(
+                        onClick = {
+                            keyboardController?.hide()
+                            scope.launch {
+                                scaffoldState.snackbarHostState.showSnackbar("Hello $text")
+                            }
+                        }
+                    ) {
+                        Text(text = "클릭!!")
+                    }
+                }
             }
+
         }
     }
 }
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
-//ComposepracticeTheme {
-//    Surface(
-//        color = MaterialTheme.colorScheme.background
-//    ) {
-//        Greeting("Android")
-//    }
-//}
-
-//@Composable
-//fun Greeting(name : String){
-//    Text(text = "Hello $name")
-//}
-//
-//
-//
-////// 박스
-//@Preview(showBackground = true)
-//@Composable
-//fun BoxPreview() {
-//    Box(
-//        modifier = Modifier
-//            .background(color = Color.Green)
-//            .fillMaxWidth()
-//            .height(200.dp),
-//    ) {
-//        Text(text = "Hello")
-//        Box(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .padding(16.dp),
-//            contentAlignment = Alignment.BottomEnd,
-//        ){
-//            Text(text = "Hello!!!!!!")
-//        }
-//    }
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun ListPreview() {
-//    val scrollState = rememberScrollState()
-//    Column(
-//        modifier = Modifier
-//            .background(color = Color.Green)
-//            .fillMaxWidth()
-//            .verticalScroll(state = scrollState)
-//    ) {
-//        for(i in 1..50){
-//            Text(text = "글씨 $i")
-//        }
-//    }
-//}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposepracticeTheme {
+        Greeting("Android")
+    }
+}
